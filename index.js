d973aa3bd362bd21a895b6750d5e166874dc3280
ebrarkesici <ebrarkesici@gmail.com>
2024-10-01 19:51:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var btk = 123;//verdigimiz ddgeri sonradand egitirebiliiyorus yine
+let btk2 = 123;//baska deger versek onu alir yani degisebiliyo
+const btk3 = 123;// degismiyo verdigimiz deger final gibi
+
+// console.log(btk3);
+
+
+//2 tur fonksiyon var
+//1.: arrow function
+const arrow_function = () => {
+
+}
+//2. normal function
+function normal_function() {
+
+}
+const topla = (a, b) => {
+    return a + b;
+}
+console.log(topla(4, 5));
